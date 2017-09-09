46c361bcd5a8f8aa1ba2d2fd5683aac0391c95d9
cla <cy.lagarde@gmail.com>
2017-09-09 15:08:10 +0200

    add dirty feature

--- POC_S3/PocS3_EAP/src/pocs3_eap/parts/common/PropertyView.java
+++ POC_S3/PocS3_EAP/src/pocs3_eap/parts/common/PropertyView.java
@@ -7,6 +7,8 @@ package pocs3_eap.parts.common;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -15,9 +17,12 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 /**
- * The class <b>BlockDiagramView</b> allows to
+ * The class <b>PropertyView</b> allows to
  */
 public class PropertyView {
+  @Inject
+  private MDirtyable dirty;
+
   @Inject
   public PropertyView() {}
 
@@ -29,8 +34,18 @@ public class PropertyView {
 
     // temporary
     new Label(parent, SWT.NONE).setText("Name :");
-    new Text(parent, SWT.NONE).setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+    Text text = new Text(parent, SWT.NONE);
+    text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+    text.addModifyListener(e -> dirty.setDirty(true));
+
     new Label(parent, SWT.NONE).setText("Value :");
-    new Text(parent, SWT.NONE).setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+    text = new Text(parent, SWT.NONE);
+    text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+    text.addModifyListener(e -> dirty.setDirty(true));
+  }
+
+  @Persist
+  public void save() {
+    dirty.setDirty(false);
   }
 }
