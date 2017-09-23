d3005e359e129c135e368e6dd7e8ebbb4df4481e
cla <cy.lagarde@gmail.com>
2017-09-23 06:51:55 +0200

    update undo/redo tooltip

--- POC_S3/PocS3_EAP/src/pocs3_eap/handlers/EditingDomainProviderUndoHandler.java
+++ POC_S3/PocS3_EAP/src/pocs3_eap/handlers/EditingDomainProviderUndoHandler.java
@@ -4,6 +4,7 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.adapter.Adapter;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -11,27 +12,52 @@ import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 
+import pocs3_eap.parts.internal.PocS3EapController;
+import pocs3_service_definitions.IEditAction;
+
 /**
  * The class <b>EditingDomainProviderUndoHandler</b> allows to.<br>
  */
 public class EditingDomainProviderUndoHandler {
   @CanExecute
-  public boolean canExecute(@Named(IServiceConstants.ACTIVE_PART) MPart mPart) {
-    if (mPart.getObject() instanceof IEditingDomainProvider) {
-      EditingDomain editingDomain = ((IEditingDomainProvider) mPart.getObject()).getEditingDomain();
-      return editingDomain.getCommandStack().canUndo();
+  public boolean canExecute(@Named(IServiceConstants.ACTIVE_PART) MPart part, Adapter adapter, PocS3EapController pocS3EapController) {
+    final Object object = part.getObject();
+
+    final IEditingDomainProvider editingDomainProvider = adapter.adapt(object, IEditingDomainProvider.class);
+    if (editingDomainProvider != null) {
+      EditingDomain editingDomain = editingDomainProvider.getEditingDomain();
+      if (editingDomain != null && editingDomain.getCommandStack().canUndo()) {
+        if (pocS3EapController != null) {
+          String undoTooltip = editingDomain.getCommandStack().getUndoCommand().getLabel();
+          pocS3EapController.setUndoTooltip(IEditAction.DEFAULT_UNDO_TOOLTIP + " : " + undoTooltip);
+        }
+
+        return true;
+      }
     }
+
+    // default tooltip
+    if (pocS3EapController != null) {
+      pocS3EapController.setUndoTooltip(IEditAction.DEFAULT_UNDO_TOOLTIP);
+    }
+
     return false;
   }
 
   @Execute
-  public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart mPart, IEventBroker eventBroker) {
-    if (mPart.getObject() instanceof IEditingDomainProvider) {
-      EditingDomain editingDomain = ((IEditingDomainProvider) mPart.getObject()).getEditingDomain();
-      editingDomain.getCommandStack().undo();
+  private static void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part, Adapter adapter, IEventBroker eventBroker) {
+    final Object object = part.getObject();
 
-      // refresh all elements UI
-      eventBroker.send(UIEvents.REQUEST_ENABLEMENT_UPDATE_TOPIC, UIEvents.ALL_ELEMENT_ID);
+    final IEditingDomainProvider editingDomainProvider = adapter.adapt(object, IEditingDomainProvider.class);
+    if (editingDomainProvider != null) {
+      EditingDomain editingDomain = editingDomainProvider.getEditingDomain();
+      if (editingDomain != null) {
+        editingDomain.getCommandStack().undo();
+
+        // refresh all elements UI
+        eventBroker.send(UIEvents.REQUEST_ENABLEMENT_UPDATE_TOPIC, UIEvents.ALL_ELEMENT_ID);
+      }
     }
   }
+
 }
